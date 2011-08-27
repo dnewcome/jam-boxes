8fa4a5ad4514a7dde33f6490fcd3aec281a9de93
dnewcome <dan@X200.(none)>
2011-08-26 17:39:05 -0700

    added client path as express static path to server.js

--- server/server.js
+++ server/server.js
@@ -5,5 +5,6 @@ var io = require('socket.io');
 var app = express.createServer();
 app.use(express.bodyParser());
 app.use(express.static( __dirname + '/static' ));
+app.use(express.static( __dirname + './../client' ));
 
 app.listen( 3000 );
