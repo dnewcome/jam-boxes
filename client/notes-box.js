91327819beba82860b83564a01c3e9c3514efbe3
Shane Tomlinson <set117@yahoo.com>
2011-08-27 14:21:38 -0700

    Making a view base class, starting a UserView, updating old views to user View.

--- client/notes-box.js
+++ client/notes-box.js
@@ -72,11 +72,9 @@ var NotesBox = (function() {
   $.extend(Box.prototype, {
     init: function(config) {
       var me=this;
-      $.extend(me, config);
-
       me.noteBoxes = [];
 
-      Shape.prototype.init.call(me, config.dndManager);
+      Shape.prototype.init.call(me, config);
 
       $(window).bind("click", me.onPaperClick.bind(me));
       me.data.on("update", me.onModelUpdate.bind(me));
---
+++ client/view.js
@@ -0,0 +1,24 @@
+// A basic user view, takes care of mute buttons, name displays, etc.
+var View = (function() {
+  var View = function(config) {
+    if(config) {
+      this.init(config);
+    }
+  };
+
+  View.prototype = new EventEmitter();
+  $.extend(View.prototype, {
+    init: function(config) {
+      var me=this;
+      $.extend(me, config);
+      this.draw();
+    },
+
+    draw: function() {
+
+    }
+  });
+
+  return View;
+}());
+
