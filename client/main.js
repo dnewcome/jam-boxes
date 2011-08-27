290e0c7e35cfc2df15279c0e069bfa8ce94f8bb0
Shane Tomlinson <set117@yahoo.com>
2011-08-27 03:41:29 -0700

    Adding two rows of measures

--- client/main.js
+++ client/main.js
@@ -10,8 +10,39 @@
   function main() {
     window.paper = Raphael('canvas', 980, 600);
 
+    createUser(0, 0);
+    createUser(1, 1);
+
+    var effectsData1 = new EffectsData(0);
+
+    var effectsStart = 192;
+    for (var i=0; i<8; i++) {
+      var effectsBox1 = new EffectsBox(effectsStart+4+(48+8)*i, 250, 48, 48, i, effectsData1);
+      effectsBoxRegistry.boxes.push(effectsBox1);
+    }
+
+    var effectsData2 = new EffectsData(1);
+    var effectsBox2 = new EffectsBox(400, 400, 48, 48, 0, effectsData2);
+
+    for (var i=0; i<effectsData2.notesPerBox*effectsData2.unitsPerNote; i++) {
+      var percent = i/(effectsData2.notesPerBox*effectsData2.unitsPerNote);
+      effectsData2.values[i] = [percent, percent];
+    }
+
+    effectsBoxRegistry.boxes.push(effectsBox2);
+
+    function effectsTick() {
+      effectsData1.tick();
+      var t = setTimeout(effectsTick, 10);
+    }
+
+    effectsTick();
+  }
+
+
+  function createUser(ownerId, rowIndex) {
     var notesData = new Model({
-      ownerId: 0
+      ownerId: ownerId
     });
 
     for (var i = 0; i < MEASURES; ++i) {
@@ -20,7 +51,7 @@
         ind: i * 4,
         paper: paper,
         xpos: LEFT_MARGIN + i * (48 + MEASURE_MARGIN),
-        ypos: 30,
+        ypos: 50 + (rowIndex * (50 + 20)),
         width: BOX_OUTER_WIDTH,
         height: BOX_OUTER_HEIGHT,
         innerWidth: BOX_INNER_WIDTH,
@@ -29,30 +60,6 @@
 		  effectsBoxRegistry.boxes.push(measureBox);
     }
 
-	var effectsData1 = new EffectsData(0);
-
-	var effectsStart = 192;
-	for (var i=0; i<8; i++) {
-		var effectsBox1 = new EffectsBox(effectsStart+4+(48+8)*i, 250, 48, 48, i, effectsData1);
-		effectsBoxRegistry.boxes.push(effectsBox1);
-	}
-
-	var effectsData2 = new EffectsData(1);
-	var effectsBox2 = new EffectsBox(400, 400, 48, 48, 0, effectsData2);
-
-	for (var i=0; i<effectsData2.notesPerBox*effectsData2.unitsPerNote; i++) {
-		var percent = i/(effectsData2.notesPerBox*effectsData2.unitsPerNote);
-		effectsData2.values[i] = [percent, percent];
-	}
-
-	effectsBoxRegistry.boxes.push(effectsBox2);
-
-	function effectsTick() {
-		effectsData1.tick();
-		var t = setTimeout(effectsTick, 10);
-	}
-
-	effectsTick();
   }
 
   window.main = main;
