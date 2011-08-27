a8a851c82b45ec344eea405acd26d9d7475fafda
dnewcome <dan@X200.(none)>
2011-08-26 17:34:39 -0700

    added static file location served up by express

--- server/server.js
+++ server/server.js
@@ -5,6 +5,5 @@ var io = require('socket.io');
 var app = express.createServer();
 app.use(express.bodyParser());
 app.use(express.static( __dirname + '/static' ));
-uuid = require('node-uuid');
 
 app.listen( 3000 );
