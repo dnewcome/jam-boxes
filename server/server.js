1bc0a3eb0b7ec52d72b3c46bfc9b8e629675aca7
dnewcome <dan@X200.(none)>
2011-08-26 19:55:47 -0700

    initial message broadcast stuff - jam join message

--- server/server.js
+++ server/server.js
@@ -2,13 +2,26 @@ var express = require('express');
 var http = require('http');
 var io = require('socket.io');
 
-var app = express.createServer();
+var app = express.createServer()
+  , io = io.listen(app);
+
+io.sockets.on('connection', function( client ) {
+    client.on('broadcast', function( data ) {
+        console.log( 'socket.io broadcast ' + data );
+		client.broadcast.to( data.jam ).emit( 'broadcast', data );
+    });
+    client.on( 'join', function( data ) {
+        client.join( data );
+        console.log( 'client joining  ' + data );
+    });
+});
+
 app.use(express.bodyParser());
 app.use(express.static( __dirname + '/static' ));
 app.use(express.static( __dirname + './../client' ));
 
 app.get('/', function(req, res){
-    res.render( 'hello.jade' );
+    res.render( 'jams.jade', { jams:['one','two']} );
 });
 
 app.listen( 3000 );
