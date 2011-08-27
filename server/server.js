8023811d7573e22bf1e0c539e51b913480e5bc33
dnewcome <dan@X200.(none)>
2011-08-26 18:40:16 -0700

    added jade templates to package.json. Added a layout and a jams template view under views.

--- server/server.js
+++ server/server.js
@@ -7,4 +7,8 @@ app.use(express.bodyParser());
 app.use(express.static( __dirname + '/static' ));
 app.use(express.static( __dirname + './../client' ));
 
+app.get('/', function(req, res){
+    res.render( 'hello.jade' );
+});
+
 app.listen( 3000 );
