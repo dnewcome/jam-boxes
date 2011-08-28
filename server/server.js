acddd413025c69dd54a4251b52cfd76264a62f28
Linode deployment user <deploy@stonecoldhackin.com>
2011-08-28 16:27:25 -0400

    update to server.js for deployment - drops root privs if run as root

--- server/server.js
+++ server/server.js
@@ -190,4 +190,14 @@ app.get('/', function(req, res){
     res.render( 'jams.jade', { jams:jams } );
 });
 
-app.listen( 3000 );
+// app.listen( 3000 );
+app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
+  console.log('Ready');
+
+  // if run as root, downgrade to the owner of this file
+  if (process.getuid() === 0)
+    require('fs').stat(__filename, function(err, stats) {
+      if (err) return console.log(err)
+      process.setuid(stats.uid);
+    });
+});
