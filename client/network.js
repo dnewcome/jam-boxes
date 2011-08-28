9783d5488cad2f022439bc7c4fe02b04656c5746
Shane Tomlinson <set117@yahoo.com>
2011-08-28 13:08:14 -0700

    We can now specify the jamid in the URL.

--- client/network.js
+++ client/network.js
@@ -1,7 +1,20 @@
 /*globals io: true */
 var Network = (function() {
+  function getUrlVars() {
+    var vars = {}, hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  }
+
+
   var Network = function() {
-    var jamid = 'pimpin';
+    var urlParams = getUrlVars();
+    var jamid = urlParams['jamid'] || undefined;
     var socket = io.connect();
 
     socket.emit('join', { jamid: jamid });
