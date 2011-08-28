d6f54402ac5ebaafe9950ffea0ebaa42ae47ea0e
Jeremy Flores <i.am@jeremyflor.es>
2011-08-28 11:19:47 -0700

    Uncommenting third instrument in audio engine config

--- client/audioengineconfig.js
+++ client/audioengineconfig.js
@@ -3,14 +3,14 @@ var ae = new AudioEngine();
 var SAMPLES = [
   'bass',
   'guitar'
-//  'snare'
+  'snare'
 ];
 
 ae.overrideProviderKey = SAMPLES[0];	// this should be the SAMPLE name for the local user
 
 // TODO: we are hard coding the instruments here
 ae.createSampler( 'guitar', '../audio/guitar.wav' );
-//ae.createSampler( 'snare', '../audio/sn.wav' );
+ae.createSampler( 'snare', '../audio/sn.wav' );
 ae.createSampler( 'bass', '../audio/101-BS02.WAV' );
 
 function start() {
