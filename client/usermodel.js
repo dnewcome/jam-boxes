710fecda0a9af4a16dda5d1b2a17bd79612d8cda
Shane Tomlinson <set117@yahoo.com>
2011-08-28 13:23:32 -0700

    Save the local users name off so we don't keep re-asking it.

--- client/usermodel.js
+++ client/usermodel.js
@@ -8,6 +8,19 @@ var UserModel = (function() {
   UserModel.prototype = new Model();
   $.extend(UserModel.prototype, {
     constructor: UserModel,
+    init: function(config) {
+      var me=this;
+      if(!config.values.ownerId) {
+        me.on("update:name", me.onLocalUserNameChange.bind(me));
+
+        var values = config.values;
+        if(localStorage.localUserName) {
+          values.name = localStorage.localUserName;
+        }
+      }
+      Model.prototype.init.call(me, config);
+    },
+
     updateFromData: function(data) {
       var me=this;
 
@@ -15,6 +28,10 @@ var UserModel = (function() {
       me.setVal("userid", data.userid);
       me.notesModel.copy(0, data.notes);
       me.effectsModel.copy(0, data.effects);
+    },
+
+    onLocalUserNameChange: function(key, name) {
+      localStorage.localUserName = name;
     }
   });
 
