1d0520cd34bb067c81dce39b1b0a410a5429c704
Shane Tomlinson <set117@yahoo.com>
2011-08-28 11:31:06 -0700

    We have multi room join/sync support now!

--- server/server.js
+++ server/server.js
@@ -8,32 +8,138 @@ var express = require('express'),
     app = express.createServer(),
     sio = io.listen(app);
 
+var MAX_USERS_PER_ROOM = 4;
+
+// the current jamid.
+var currJamID = 1;
+
+// a container of all the jams.
 var jams = {};
 
-sio.sockets.on('connection', function( socket ) {
-    var userid;
-    socket.on('broadcast', function( data ) {
-      //console.log( 'socket.io broadcast ' + data );
-      data.userid = userid;
+// a lookup table of jams for each socket.
+var socketToJams = {};
+
+function getSocketId(socket) {
+  return socket.id;
+}
+
+function getJamIdForSocket(socket) {
+  var socketid = getSocketId(socket);
+  var jamInfo = socketToJams[socketid];
+  return jamInfo && jamInfo.jamid;
+}
+
+function getJamInfoForSocket(socket) {
+  return socketToJams[socket.id];
+};
+
+function getAvailableJam() {
+  var jamid, jam;
+
+  // do a simple search through the jams for ones that are not yet full.
+  for (jamid in jams) {
+    jam = jams[jamid];
+
+    if(jam.userCount < MAX_USERS_PER_ROOM) {
+      // return immediately so that we know we found one and do not create a
+      // new one.
+      return jam;
+    }
+  }
+
+ jam = createJam();
+
+  return jam;
+}
+
+
+function getJam(jamid) {
+  var jam = jams[jamid];
+  return jam
+}
+
+function createJam() {
+  // create a new jam
+  var jamid = '_' + currJamID;
+  currJamID++;
+
+  jam = {
+    id: jamid,
+    name: 'Unmamed Jam',
+    userCount: 0
+  };
+
+  jams[jamid] = jam;
+
+  return jam;
+};
+
+function socketJoinJam(socket) {
+  var jamid = getJamIdForSocket(socket),
+      jam;
+
+  if(jamid) {
+    console.log("re-joining old jam");
+    jam = getJam(jamid);
+  }
+  else {
+    console.log("searching for available jam");
+    // user is not part of a jam, add htem to one.
+    var jam = getAvailableJam();
+
+    var userid = jam.userCount;
+
+    // associate the socket with a jam
+    var socketid = getSocketId(socket);
+    socketToJams[socketid] = {
+      jamid: jam.id,
+      userid: userid
+    };
+
+    jam.userCount++;
+  }
+
+  return jam;
+}
+
+function socketLeaveJam(socket) {
+  var jamid = getJamIdForSocket(socket);
+
+  if (jamid) {
+    console.log("leaving jam: " + jamid);
+    var jam = getJam(jamid);
+    jam.userCount--;
+
+    delete socketToJams[getSocketId(socket)];
+  }
+}
+
+sio.sockets.on('connection', function(socket) {
+  var userid;
+  socket.on('broadcast', function(data) {
+    console.log("broadcast received from: " + socket.id);
+    var jamInfo = getJamInfoForSocket(socket);
+
+    if(jamInfo) {
+      data.userid = jamInfo.userid;
       if(data.name === "Enter your name") {
         data.name = "Unknown User";
       }
-      socket.broadcast.to( data.roomid ).emit( 'userupdate', data );
-    });
 
-    socket.on( 'join', function(data) {
-        var roomid = data.roomid;
+      console.log("broadcasting to: " + jamInfo.jamid);
+      socket.broadcast.to(jamInfo.jamid).emit('userupdate', data);
+    }
+});
 
-        var jam = jams[roomid] = jams[roomid] || {
-          name: 'Unmamed Jam',
-          userCount: 0
-        };
+  socket.on('join', function(data) {
+    var jam = socketJoinJam(socket);
 
-        userid = jam.userCount;
-        jam.userCount++;
+    socket.join(jam.id);
+  });
 
-        socket.join( roomid );
-    });
+  socket.on('disconnect', function(data) {
+    socketLeaveJam(socket);
+  });
 });
 
 app.configure(function() {
