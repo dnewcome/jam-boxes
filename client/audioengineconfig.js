e8db1426d75a93b4669d4281540ca21d82cf0003
Shane Tomlinson <set117@yahoo.com>
2011-08-27 18:15:52 -0700

    Adding an array of samples so we don't have to directly reference them.

--- client/audioengineconfig.js
+++ client/audioengineconfig.js
@@ -1,10 +1,14 @@
 var ae = new AudioEngine();
 
+var SAMPLES = [
+  'cymbal',
+  'snare'
+];
 // TODO: we are hard coding the instruments here
 ae.createSampler( 'cymbal', '../audio/1.wav' );
 ae.createSampler( 'snare', '../audio/sn.wav' );
 
-function start() { 
+function start() {
 	ae.start();
 }
 function stop() {
